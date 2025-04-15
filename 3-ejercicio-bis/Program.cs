0a2791268287521553bbaf52fdbd01ee58646b1f
nahuedintal <nack3rz@proton.me>
2025-04-15 16:36:10 -0300

    3 ejercicio con variación de complejidad

---
+++ 3-ejercicio-bis/Program.cs
@@ -0,0 +1,21 @@
+﻿// Ingresar por teclado el precio, cantidad y el porcentaje de descuento y calcular el total a pagar
+
+Console.WriteLine(":: Descuento de Venta!");
+
+Console.WriteLine(":: Ingrese precio!");
+double precio = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine(":: Ingrese la cantidad de productos!");
+int cantidad = Convert.ToInt32(Console.ReadLine());
+
+double subtotal = precio * cantidad;
+Console.WriteLine(subtotal);
+
+Console.WriteLine(":: Ingrese el porcentaje de descuento!");
+double porcentaje = Convert.ToDouble(Console.ReadLine());
+
+double descuento = (subtotal * porcentaje / 100);
+Console.WriteLine(descuento);
+
+double total = (subtotal - descuento);
+Console.WriteLine(total);
