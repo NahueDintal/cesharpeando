14ddaf6d06ec975696928d30e9605298b931ce36
nahuedintal <nack3rz@proton.me>
2025-04-18 11:02:51 -0300

    editado y comentado del 8vo ejercicio

--- 8-ejercicio/Program.cs
+++ 8-ejercicio/Program.cs
@@ -2,25 +2,23 @@
 //durante un mes y el valor de la hora y calcular el salario
 //correspondiente. Teniendo en cuenta que si supera las 160 horas
 //trabajadas se deberá abonar el doble del valor del precio por hora
-
-// Ingreso
-
+// Inicio
 Console.Write(":: Ingresa la cantidad de horas del mes!");
-double horas = Convert.ToDouble(Console.ReadLine());
-
 Console.Write(":: Ingresa el valor de hora de trabajo!");
+// Entrada
 double valor = Convert.ToDouble(Console.ReadLine());
-
-// Proceso
-
+double horas = Convert.ToDouble(Console.ReadLine());
+// Condicional discriminando la entrada
 if (horas >= 160 )
 {
+  // Proceso
   double totalpordos = horas * valor * 2;
-  //Salida
-  Console.WriteLine($":: El salario es de: ${totalpordos}");
+  // Salida
+  Console.WriteLine($":: El salario corresponde a superar las 160hs de trabajo mensuales: ${totalpordos}");
 } else {
+  // Proceso
   double total = horas * valor;
-  //Salida
-  Console.WriteLine(total);
+  // Salida
+  Console.WriteLine($":: El salario total por la cantidad de horas de es: ${total}");
 }
-
+// Fin!
