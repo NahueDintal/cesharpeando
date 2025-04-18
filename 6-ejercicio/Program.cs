0da106c91ca7d74d18c4c616c3ce6c55498663e6
nahuedintal <nack3rz@proton.me>
2025-04-18 10:38:09 -0300

    comentado y editado del 6to ejercicio

--- 6-ejercicio/Program.cs
+++ 6-ejercicio/Program.cs
@@ -1,19 +1,17 @@
 ﻿//Hacer un programa que permita ingresar una cantidad de pesos y me
 // calcule la cantidad de dólares, euros y reales que equivale dicha
-// cantidad ingresada-
-
+// cantidad ingresada.
+// Inicio
 // Ingreso
 Console.WriteLine(":: Por favor ingrese la cantidad de pesos a calcular");
 double pesos = Convert.ToDouble(Console.ReadLine());
-
 double dolares = 1097;
 double reales = 194;
 double euros = 1302;
-
 // Proceso
 double converdolares = dolares * pesos;
 double converreales = reales * pesos;
 double convereuros = euros * pesos;
-
 // Salida
-Console.WriteLine($"\n :: Valor en Dolares {converdolares}, Valor en Reales {converreales}, Valor en Euros {convereuros}");
+Console.WriteLine($":: Valor en Dolares ${converdolares}, Valor en Reales ${converreales}, Valor en Euros ${convereuros}");
+// Fin!
