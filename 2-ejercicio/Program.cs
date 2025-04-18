1359eb09a7a53457d1ec77d897fc2bbc0c663891
nahuedintal <nack3rz@proton.me>
2025-04-18 12:05:24 -0300

    actualizado de 2do ejercicio

---
+++ 2-ejercicio/Program.cs
@@ -0,0 +1,16 @@
+﻿//Ingresar por teclado la base y altura de un rectángulo
+//y calcular e imprimir el perímetro y arrea correspondiente.
+// Inicio
+Console.WriteLine(":: Calculadora de Area y perímetro!");
+// Entrada
+Console.WriteLine(":: Ingresa los valores de base!");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(":: Ingresa los valores de altura!");
+double h = Convert.ToDouble(Console.ReadLine());
+// Proceso
+double area = b * h;
+double perimetro = 2 * ( b + h );
+// Salida
+Console.WriteLine($":: El resultado de area es: {area}");
+Console.WriteLine($":: El resultado de perimetro es: {perimetro}");
+// Fin
